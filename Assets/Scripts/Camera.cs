e15c4c6cd7ce854df55bac2fd71583eaddbe2f7c
Ian Fisher <loktar@gmail.com>
2019-02-17 19:45:53 -0800

    Initial commit
    Added a room with a player that can move and jump

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Player player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+        LookUpOrDown();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 playerPosition = player.transform.position;
+        float playerHeight = player.transform.localScale.y;
+        float halfPlayerHeight = playerHeight / 2f;
+
+        this.transform.position = playerPosition + new Vector3(0, halfPlayerHeight, 0);
+        this.transform.eulerAngles = new Vector3(transform.eulerAngles.x, player.transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    private void LookUpOrDown()
+    {
+        float rotateVertical = Input.GetAxis("Tilt Head");
+        Vector3 rotationDelta = new Vector3(rotateVertical * player.rotationSpeed * Time.deltaTime, 0, 0);
+
+        this.transform.eulerAngles += rotationDelta;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float rotationSpeed = 150;
+    public float jumpStrength = 10;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+        RotatePlayer();
+        MaybeJump();
+    }
+
+    private void MovePlayer()
+    {
+        float moveForward = Input.GetAxis("Vertical");
+        float moveSideways = Input.GetAxis("Horizontal");
+
+        this.transform.position += this.transform.forward * moveForward * movementSpeed * Time.deltaTime;
+        this.transform.position += this.transform.right * moveSideways * movementSpeed * Time.deltaTime;
+    }
+
+    private void RotatePlayer()
+    {
+        float rotateHorizontal = Input.GetAxis("Rotate Player");
+
+        this.transform.eulerAngles += new Vector3(0, rotateHorizontal, 0) * rotationSpeed * Time.deltaTime;
+    }
+
+    private void MaybeJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector3(0, jumpStrength, 0));
+        }
+    }
+}
